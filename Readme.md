84320855eece53dc86aab95835abc4806455a6b4
ido karelitz <idoinfo@gmail.com>
2022-04-11 10:08:24 +0300

    Initial Commit

---
+++ Readme.md
@@ -0,0 +1,8 @@
+installs needed
+---------------
+1. Flask 2.1.1 -- pip install Flask
+
+2.Sqlalchemy 2.5.1 -- pip install -U Flask-SQLAlchemy
+
+3.Flask-wtf 1.0.1/Wtforms 3.0.1 -- pip install Flask-WTF
+
---
+++ app.py
@@ -0,0 +1,6 @@
+import datetime
+from flask import Flask, render_template, flash, request, redirect, url_for
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, PasswordField, BooleanField, ValidationError
+from wtforms.validators import DataRequired, EqualTo, Length
+from flask_sqlalchemy import SQLAlchemy
