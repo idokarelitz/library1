1b21c64da4e626c1e7905ad9eaaaab141f128f33
ido karelitz <idoinfo@gmail.com>
2022-04-11 12:58:09 +0300

    flask run

--- app.py
+++ app.py
@@ -4,3 +4,35 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField, BooleanField, ValidationError
 from wtforms.validators import DataRequired, EqualTo, Length
 from flask_sqlalchemy import SQLAlchemy
+
+
+
+# Create a Flask Instance
+app = Flask(__name__)
+
+
+
+
+# Create a route decorator
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+# Create Custom Error Pages
+# Invalid URL
+@app.errorhandler(404)
+def page_not_found(e):
+	return render_template("404.html"), 404
+
+# Internal Server Error
+@app.errorhandler(500)
+def page_not_found(e):
+	return render_template("500.html"), 500
+
+#start app
+if __name__=='__main__':
+    app.run(debug=True,port=9000)
